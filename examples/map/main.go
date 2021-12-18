3f64559f4dd47aee53c8b2c150627f0dcd925840
kylenzhang <kungvwow@126.com>
2021-12-18 14:27:35 +0800

    init

--- examples/map/main.go
+++ examples/map/main.go
@@ -32,23 +32,3 @@ func main() {
 
 	fmt.Println(res)
 }
-
-// if concurrency = 1
-// max work lasts 4 secs , task t lasts 4secs
-//
-// if concurrency = 2
-//	time	worker1							worker2
-// 	0			task1								task2
-//
-//	1s		task3								task4
-//
-//	2s		task3								task4
-//
-// 	3s		task3								task4
-//
-// 	4s		task3								task4
-//
-// 	5s												task4-done
-//
-// total: 5secs
-//
